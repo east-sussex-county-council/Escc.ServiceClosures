f2d3c8b9fe7f5fd329d041abf1924b0e1e3a6228
richardm <richard.mason@eastsussex.gov.uk>
2014-08-15 17:59:15 +0100

    Swop project reference for NuGet reference

--- Moderator.cs
+++ Moderator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Net.Mail;
-using EsccWebTeam.Gdsc;
+using Escc.AddressAndPersonalDetails;
 
 namespace EsccWebTeam.ServiceClosures
 {
