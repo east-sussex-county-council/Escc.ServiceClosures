5a1320ebfcf01eceb19e40475a59c1448d1583ee
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-07-13 18:18:10 +0100

    Add a tests project. Project needs refactor to be more testable.

---
+++ Escc.ServiceClosures.Tests/ServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Escc.ServiceClosures.Tests
+{
+    [TestFixture]
+    public class ServiceTests
+    {
+        [Test]
+        public void ClosuresTodayReturnsOneResult()
+        {
+            var service = new Service();
+            service.Closures.Add(new Closure() { StartDate = DateTime.Today.AddDays(-1), EndDate = DateTime.Today.AddDays(-1), Status = ClosureStatus.Closed });
+            service.Closures.Add(new Closure() { StartDate = DateTime.Today, EndDate = DateTime.Today, Status = ClosureStatus.Closed });
+            service.Closures.Add(new Closure() { StartDate = DateTime.Today.AddDays(1), EndDate = DateTime.Today.AddDays(1), Status = ClosureStatus.Closed });
+
+            var closures = service.CheckForClosuresToday();
+
+            Assert.AreEqual(1, closures.Count);
+        }
+
+        [Test]
+        public void ClosuresTomorrowReturnsOneResult()
+        {
+            var service = new Service();
+            service.Closures.Add(new Closure() { StartDate = DateTime.Today.AddDays(-1), EndDate = DateTime.Today.AddDays(-1), Status = ClosureStatus.Closed });
+            service.Closures.Add(new Closure() { StartDate = DateTime.Today, EndDate = DateTime.Today, Status = ClosureStatus.Closed });
+            service.Closures.Add(new Closure() { StartDate = DateTime.Today.AddDays(1), EndDate = DateTime.Today.AddDays(1), Status = ClosureStatus.Closed });
+
+            var closures = service.CheckForClosuresTomorrow();
+
+            Assert.AreEqual(1, closures.Count);
+        }
+    }
+}
